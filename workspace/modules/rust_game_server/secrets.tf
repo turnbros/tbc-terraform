115cafd0ca366be29e2f77d8c3ce15348504a685
Dylan Turnbull <dylanturn@gmail.com>
2021-08-30 10:30:04 -0500

    updated the rust rcon password secrets name

--- workspace/modules/rust_game_server/secrets.tf
+++ workspace/modules/rust_game_server/secrets.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_secret" "rcon_password" {
   metadata {
-    name      = "rcon-password"
+    name      = local.server_name
     namespace = var.tenant_namespace
   }
   data = {
