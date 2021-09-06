0540edb9d9a8e97a674ffb0d3d15707d4850f17b
Dylan Turnbull <dylanturn@gmail.com>
2021-09-05 21:39:58 -0500

    added another gig to the persistent volume

--- workspace/modules/rust_game_server/volume.tf
+++ workspace/modules/rust_game_server/volume.tf
@@ -13,7 +13,7 @@ resource "kubernetes_persistent_volume_claim" "rust_volume" {
     access_modes       = ["ReadWriteMany"]
     resources {
       requests = {
-        storage = "8Gi"
+        storage = "9Gi"
       }
     }
   }
