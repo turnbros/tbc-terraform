0e34bb38c968ff097603b8892c2989bb646aca4f
Dylan Turnbull <dylanturn@gmail.com>
2021-09-10 18:13:05 -0500

    removed old output

--- workspace/tenant_configuration/outputs.tf
+++ workspace/tenant_configuration/outputs.tf
@@ -1,6 +1,3 @@
 output "namespace_name" {
   value = kubernetes_namespace.tenant_namespace.metadata.0.name
 }
-output "tenant_firewall_alias_id" {
-  value = opnsense_firewall_alias.tenant_firewall_alias.id
-}
